a21620f7be2f308c0e311568c27cc607d8ebfee3
Danielle Cardona <danielle.cardona23@gmail.com>
2025-02-02 19:19:43 -0700

    delete unused toggler

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Style from './Navbar.module.scss';
-import Toggler from "./home/Toggler";
 import { HashLink as Link } from 'react-router-hash-link';
 import { Box } from "@mui/material";
 import { info } from "../info/Info";
